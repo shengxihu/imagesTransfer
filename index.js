05eafc2bfef912da976cd65bc8500957a1985948
shengxihu <1679112936@qq.com>
2016-04-05 22:52:10 +0800

    实现图片预览

--- index.js
+++ index.js
@@ -5,4 +5,21 @@ function uploadControl(){
 		image_url.value=uparea.value;
 	})
 }
+function imagePreview(){
+	var imgReader = new FileReader(),
+	uparea=document.getElementById("uparea");
+	imgReader.onload = function (imgReader) {
+  		var image_preview=document.getElementById("image-preview");
+  		image_preview.src = imgReader.target.result;
+	};
+	uparea.addEventListener("change",function(){
+		loadImageFile();
+	})
+	function loadImageFile() {
+	  if (document.getElementById("uparea").files.length === 0) { return; }
+	  var file = document.getElementById("uparea").files[0];
+	  imgReader.readAsDataURL(file);
+	}
+}
 uploadControl();
+imagePreview();
